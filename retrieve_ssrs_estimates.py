09f3658d62d7653dc3417bd1cd1f7fc3b150eb5a
Clint Daniels <clint.daniels@sandag.org>
2017-07-26 08:47:17 -0700

    Update queries and command lines to make more user friendly.

--- retrieve_ssrs_estimates.py
+++ retrieve_ssrs_estimates.py
@@ -7,9 +7,9 @@ from ntlm import HTTPNtlmAuthHandler
 from time import strftime
 
 if len(sys.argv) != 5:
-    sys.exit("Must provide datasource_id, geotype, user, and password")
+    sys.exit("Must provide year, geotype, user, and password")
 
-datasource_id = sys.argv[1] 
+year = sys.argv[1] 
 geotype = sys.argv[2]
 
 #Windows username and password to access report server
@@ -23,21 +23,24 @@ db_user = user
 db_password = password
  
 
-output_path = 'estimates_{0}_{1}'.format(datasource_id, strftime('%Y_%m_%d'))
+output_path = 'estimates_{0}_{1}'.format(year, strftime('%Y_%m_%d'))
 
 if not (os.access(output_path, os.F_OK)):
     os.mkdir(os.getcwd() + '/' + output_path)
+
+if not (os.access(output_path + '/' + geotype, os.F_OK)):
     os.mkdir(os.getcwd() + '/' + output_path + '/' + geotype)
 
 conn = pymssql.connect(host=db_server, user=db_user, password=db_password, database=db_name)
 cur = conn.cursor()
 
-geo_sql = "SELECT lower(REPLACE(RTRIM(mgra.geozone), ' ', '%20')), ds.yr, ds.datasource_id FROM demographic_warehouse.dim.datasource ds JOIN demographic_warehouse.dim.mgra mgra ON ds.series = mgra.series WHERE ds.yr = {0} and mgra.geotype = '{1}' and ds.datasource_type_id = 2 and vintage = 2015 GROUP BY mgra.geozone, ds.yr, ds.datasource_id".format(datasource_id, geotype)
+geo_sql = "SELECT RTRIM(m.geozone),ds.datasource_id FROM demographic_warehouse.dim.datasource ds INNER JOIN dim.mgra m ON ds.series = m.series WHERE ds.datasource_type_id = 2 and is_active = 1 and publish_year = 2016 and m.geotype = '{0}' and RTRIM(geozone) is not null GROUP BY m.geozone, ds.datasource_id".format(geotype)
 
 cur.execute(geo_sql)
 
 for row in cur:
-    url = 'http://sql2014a8/ReportServer?%2festimates%2fEstimate+Profile_sr13_sql2014a8&GEOTYPE={0}&GEOZONE={1}&DATASOURCE_ID={2}&rs:Format=PDF'.format(geotype, row[0], row[2])
+
+    url = 'http://sql2014a8/ReportServer?%2festimates%2fEstimate+Profile_sr13_sql2014a8&GEOTYPE={0}&GEOZONE={1}&DATASOURCE_ID={2}&YEAR={3}&rs:Format=PDF'.format(geotype, urllib2.quote(row[0]), row[1], year)
     
     print url
     
@@ -52,6 +55,9 @@ for row in cur:
 
     pagehandle = urllib2.urlopen(url)
 
-    localFile = open('{0}\\sandag_estimate_{1}_{2}_{3}.pdf'.format(output_path, row[1],geotype, row[0].replace('%20',' ')), 'wb')
+    outpath = '{0}\\{1}\\sandag_estimate_{2}_{3}_{4}.pdf'.format(output_path, geotype, year,geotype, row[0].replace('%20',' '))
+    print outpath
+    
+    localFile = open(outpath, 'wb')
     localFile.write(pagehandle.read())
     localFile.close()
